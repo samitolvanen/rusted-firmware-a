88e994fb69fac87ccde810c77d1cd88dcd348cdf
Tomás González <tomasagustin.gonzalezorlando@arm.com>
2025-06-13 13:50:20 +0100

    add something
    Change-Id: Ifb1c2eccd32c3deecff04f390f4ab39bf41ecf6d
    Signed-off-by: Tomás González <tomasagustin.gonzalezorlando@arm.com>

--- rust/src/sysregs.rs
+++ rust/src/sysregs.rs
@@ -129,6 +129,11 @@ macro_rules! write_sysreg {
                     options(nostack),
                     value = in(reg) value,
                 );
+                asm!(
+                    "nop",
+                    // asd
+                    options(nomem, nostack, preserves_flags),
+                );
             }
         }
     };
