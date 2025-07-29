fc42fac86c943c1ae83da222ba3a5737582576d2
Andrew Walbran <qwandor@google.com>
2025-07-29 15:53:46 +0100

    refactor(rust): build cpu_data.S from Rust
    This was the last assembly file being built from the build script, so
    removing it lets us remove all the assembly headers and build
    infrastructure too.
    
    Change-Id: I59487ff593b98f649b2db08e819ebeff7a018e93
    Signed-off-by: Andrew Walbran <qwandor@google.com>

--- platforms.rs
+++ platforms.rs
@@ -2,32 +2,4 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-mod fvp;
-mod qemu;
-
-use cc::Build;
-use fvp::FvpBuilder;
-use qemu::QemuBuilder;
-use std::error::Error;
-
-pub const PLATFORMS: [&str; 2] = [QemuBuilder::PLAT_NAME, FvpBuilder::PLAT_NAME];
-
-type BuildResult = Result<(), Box<dyn Error>>;
-
-pub trait Builder {
-    /// Sets up platform-specific configurations (code generation, file inclusions, `cc::Build`
-    /// definitions, etc.).
-    fn configure_build(&self, build: &mut Build) -> BuildResult;
-}
-
-pub fn get_builder(platform: &str) -> Result<Box<dyn Builder>, Box<dyn Error>> {
-    match platform {
-        FvpBuilder::PLAT_NAME => Ok(Box::new(FvpBuilder)),
-        QemuBuilder::PLAT_NAME => Ok(Box::new(QemuBuilder)),
-        _ => Err(format!(
-            "Unexpected platform name {:?}. Supported platforms: {:?}",
-            platform, PLATFORMS
-        )
-        .into()),
-    }
-}
+pub const PLATFORMS: &[&str] = &["fvp", "qemu"];
