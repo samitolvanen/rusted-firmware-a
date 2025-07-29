43685a46556a8991606e02bc42a07bee236524d2
Andrew Walbran <qwandor@google.com>
2025-07-29 15:53:46 +0100

    refactor(rust): build cpu_data.S from Rust
    This was the last assembly file being built from the build script, so
    removing it lets us remove all the assembly headers and build
    infrastructure too.
    
    Change-Id: I59487ff593b98f649b2db08e819ebeff7a018e93
    Signed-off-by: Andrew Walbran <qwandor@google.com>

--- build.rs
+++ build.rs
@@ -6,42 +6,8 @@
 
 mod platforms;
 
-use cc::Build;
-use platforms::{PLATFORMS, get_builder};
+use platforms::PLATFORMS;
 use std::env;
-
-fn build_libtfa(platform: &str) {
-    let platform_builder = get_builder(platform).unwrap();
-
-    // SAFETY: The build script is single-threaded.
-    unsafe {
-        env::set_var("CROSS_COMPILE", "aarch64-none-elf");
-        env::set_var("CC", "clang");
-    }
-
-    let mut build = Build::new();
-    if env::var("CARGO_FEATURE_RME").as_deref() == Ok("1") {
-        build.define("ENABLE_RME", Some("1"));
-    }
-    build
-        .define("CRASH_REPORTING", Some("1"))
-        .define("ENABLE_ASSERTIONS", Some("1"))
-        .include("include")
-        .include("include/arch/aarch64")
-        .include("include/lib/cpus/aarch64")
-        .include("include/lib/el3_runtime/aarch64")
-        .include("include/lib/libc")
-        .include("include/plat/arm/common/aarch64")
-        .file("cpu_data.S");
-
-    if let Ok(debug) = env::var("DEBUG") {
-        build.define("DEBUG", debug.as_str());
-    }
-
-    platform_builder.configure_build(&mut build).unwrap();
-
-    build.compile("tfa");
-}
 
 fn setup_linker(platform: &String) {
     println!("cargo:rustc-link-arg=-Tbl31.ld");
@@ -67,8 +33,6 @@ fn main() {
 
     if env::var("CARGO_CFG_TARGET_OS").unwrap() == "none" {
         let platform = env::var("CARGO_CFG_PLATFORM").expect("Missing platform name");
-
-        build_libtfa(&platform);
 
         setup_linker(&platform);
     }
