ae77fabdb39deba78c0159f2dfa78c9abcf80d09
Andrew Walbran <qwandor@google.com>
2025-07-30 15:34:22 +0100

    refactor: add generic struct for per-world data
    This generalises the existing CpuState struct.
    
    PER_WORLD_CONTEXT doesn't need to be mutable, it is only ever read.
    
    Change-Id: Ib13650b9f0b676c13d14dbce5d1a059230ad0f73
    Signed-off-by: Andrew Walbran <qwandor@google.com>

--- src/services.rs
+++ src/services.rs
@@ -182,9 +182,7 @@ impl Services {
 
         if step_to_next_instr {
             exception_free(|token| {
-                cpu_state(token)
-                    .context_mut(world)
-                    .skip_lower_el_instruction();
+                cpu_state(token)[world].skip_lower_el_instruction();
             })
         }
     }
