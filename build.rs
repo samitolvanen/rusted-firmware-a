b03957a5e6a7150ad4c38a0d269dfedc1ea7e6d8
Snehal Koukuntla <snehalreddy@google.com>
2025-07-30 17:03:33 +0000

    build(rust): Make build process vendor layer compatible
    The build process will work irrespective of how the platforms are
    arranged. Currently it requires that all platforms are arranged in a
    flat list in platorms/. This patch gives us the option to organise them
    in platforms/{vendor}/{board}/. This does assume that platform names are
    unique, which is fair. C TF-A also assumes the same.
    
    Change-Id: I1ae00645711ee6e5b863368fbd1f627541941e50
    Signed-off-by: Snehal Koukuntla <snehalreddy@google.com>

--- build.rs
+++ build.rs
@@ -52,13 +52,24 @@ fn setup_linker(platform: &String) {
     // Select the linker scripts. bl31.ld is common to all platforms. It gets supplemented by the
     // platform linker script. Some platforms have multiple linker scripts, depending on the enabled
     // features.
-    let linker_name = platform.clone();
+    let linker_name_base = platform.clone();
     #[cfg(feature = "rme")]
-    let linker_name = linker_name + "-rme";
+    let linker_name_base = linker_name_base + "-rme";
 
-    let linker_name = format!("platforms/{}/{}.ld", platform, linker_name);
-    println!("cargo:rustc-link-arg=-T{}", linker_name);
-    println!("cargo:rerun-if-changed={}", linker_name);
+    let vendor_path = env::var("CARGO_CFG_VENDOR_PATH").unwrap_or_default();
+
+    let mut linker_path = std::path::PathBuf::from("platforms");
+    if !vendor_path.is_empty() {
+        linker_path.push(vendor_path);
+    }
+    linker_path.push(platform);
+    let linker_file = format!("{}.ld", linker_name_base);
+    linker_path.push(linker_file);
+
+    let linker_script = linker_path.to_str().expect("Invalid linker path");
+
+    println!("cargo:rustc-link-arg=-T{}", linker_script);
+    println!("cargo:rerun-if-changed={}", linker_script);
 }
 
 fn main() {
@@ -66,6 +77,7 @@ fn main() {
         "cargo::rustc-check-cfg=cfg(platform, values(\"{}\"))",
         PLATFORMS.join("\", \""),
     );
+    println!("cargo::rustc-check-cfg=cfg(vendor_path)");
 
     if env::var("CARGO_CFG_TARGET_OS").unwrap() == "none" {
         let platform = env::var("CARGO_CFG_PLATFORM").expect("Missing platform name");
