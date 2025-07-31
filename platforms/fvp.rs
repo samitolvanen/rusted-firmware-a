f5845a075f25e9ce2ba7cfd45709689bc182ed95
Mateusz Sulimowicz <matsul@google.com>
2025-07-31 15:37:36 +0000

    refactor(rust): pass BL31_BASE and BL31_SIZE to common build script
    Change-Id: I1a47bf9ed8ca50b5c64f474bc7127a651fe8cb38
    Signed-off-by: Mateusz Sulimowicz <matsul@google.com>

--- platforms/fvp.rs
+++ platforms/fvp.rs
@@ -6,8 +6,20 @@ use super::Builder;
 
 pub struct FvpBuilder;
 
-impl Builder for FvpBuilder {}
-
 impl FvpBuilder {
     pub const PLAT_NAME: &str = "fvp";
+
+    const BL2_BASE: u64 = 0x4060000;
+    const BL31_BASE: u64 = 0x4003000;
+    const BL31_SIZE: u64 = Self::BL2_BASE - Self::BL31_BASE;
+}
+
+impl Builder for FvpBuilder {
+    fn bl31_base(&self) -> u64 {
+        Self::BL31_BASE
+    }
+
+    fn bl31_size(&self) -> u64 {
+        Self::BL31_SIZE
+    }
 }
