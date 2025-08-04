3400ad3cae67a028a52ed83ae047d349f2e5e800
Imre Kis <imre.kis@arm.com>
2025-08-04 15:53:33 +0200

    feat(rust): implement cache flush functions
    Add functions for reading/writing cache related registers and implement
    cache flush functions by cache levels. Also add the newly implemented
    functions to the AEM generic CPU power down sequence.
    
    Change-Id: I77435f892644aba52232f189cd03448e6b0f4e0d
    Signed-off-by: Imre Kis <imre.kis@arm.com>

--- src/cpu/aem_generic.rs
+++ src/cpu/aem_generic.rs
@@ -2,7 +2,10 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
-use log::info;
+use crate::{
+    aarch64::{disable_dcache, flush_all_caches, flush_level1_cache, flush_level2_cache},
+    sysregs::{CacheLevel, CacheType, read_clidr_el1},
+};
 
 use super::Cpu;
 
@@ -17,11 +20,30 @@ unsafe impl Cpu for AemGeneric {
         core::arch::naked_asm!("ret");
     }
 
+    /// Disables data cache, flushes level 1 and also flushes level 2 cache if level 3 cache is
+    /// present.
     fn power_down_level0() {
-        info!("AemGeneric::power_down_level0");
+        // Safety: The following function calls flush the required levels of caches to avoid data
+        // loss before powering down the core.
+        unsafe {
+            disable_dcache();
+        }
+
+        flush_level1_cache();
+
+        if read_clidr_el1().ctype(CacheLevel::new(3)) != CacheType::NoCache {
+            flush_level2_cache();
+        }
     }
 
+    /// Disables data cache and flushes level 1, level 2 and level 3 caches if present.
     fn power_down_level1() {
-        info!("AemGeneric::power_down_level1");
+        // Safety: The following function calls flush the required levels of caches to avoid data
+        // loss before powering down the core and the cluster.
+        unsafe {
+            disable_dcache();
+        }
+
+        flush_all_caches();
     }
 }
