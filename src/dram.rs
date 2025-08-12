1fccb033d196717bab615c13c297dffb8485cb45
Andrew Walbran <qwandor@google.com>
2025-08-12 13:47:16 +0100

    feat: add safe abstraction for lazily-initialised statics in DRAM
    Change-Id: Ic06f9b09337c9708d9bbe84007b1c603e29b8d14
    Signed-off-by: Andrew Walbran <qwandor@google.com>

--- src/dram.rs
+++ src/dram.rs
@@ -53,9 +53,56 @@ macro_rules! zeroed_mut {
 }
 pub(crate) use zeroed_mut;
 
+/// Declares a static lazily-initialised `$t` which may reside in zero-initialised memory.
+///
+/// For example:
+///
+/// ```
+/// lazy_indirect!(FOO, u64, 42);
+/// ```
+///
+/// will create
+///
+/// ```
+/// static FOO: Lazy<&u64> = ...;
+/// ```
+///
+/// The indirection (via the reference stored in the `Lazy`) allows the value itself to be stored in
+/// a different section of memory. Attributes can optionally be provided both for the underlying
+/// static and for the `Lazy` wrapper, e.g.:
+///
+/// ```
+/// lazy_indirect! {
+///     /// Rustdoc comment for FOO.
+///     pub FOO, u64, 42, unsafe(link_section = ".bss.dram")
+/// }
+/// ```
+#[allow(unused_macros)]
+macro_rules! lazy_indirect {
+    ($(#[$attributes:meta])* $visibility:vis $name:ident, $t:ty, $init:expr $(, $raw_attributes:meta)*) => {
+        $(#[$attributes])*
+        $visibility static $name: spin::Lazy<&$t> = spin::Lazy::new(|| {
+            $(#[$raw_attributes])*
+            static mut RAW: core::mem::MaybeUninit<$t> =
+                $crate::dram::const_zeroed();
+            // SAFETY: This is the only place where we create a reference to the contents of this
+            // static mut, and it only happens once during the initialisation of the `Lazy`.
+            unsafe { &mut *&raw mut RAW }.write($init)
+        });
+    };
+}
+pub(crate) use lazy_indirect;
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::{
+        context::PerCoreState,
+        platform::{Platform, PlatformImpl, exception_free},
+    };
+    use core::cell::RefCell;
+    use percore::{ExceptionLock, PerCore};
+    use spin::mutex::SpinMutex;
 
     #[test]
     fn use_zeroed() {
@@ -73,5 +120,36 @@ mod tests {
         assert_eq!(**test_ref, 0);
         **test_ref = 42;
         assert_eq!(**test_ref, 42);
+    }
+
+    #[test]
+    fn lazy() {
+        lazy_indirect!(TEST, u64, 42);
+        assert_eq!(**TEST, 42);
+    }
+
+    #[test]
+    fn lazy_percore() {
+        lazy_indirect!(
+            TEST_PERCORE,
+            PerCoreState<u64>,
+            PerCore::new(
+                [const { ExceptionLock::new(RefCell::new(42)) }; PlatformImpl::CORE_COUNT]
+            )
+        );
+
+        exception_free(|token| {
+            assert_eq!(*TEST_PERCORE.get().borrow_mut(token), 42);
+            *TEST_PERCORE.get().borrow_mut(token) = 55;
+            assert_eq!(*TEST_PERCORE.get().borrow_mut(token), 55);
+        });
+    }
+
+    #[test]
+    fn lazy_mutex() {
+        lazy_indirect!(TEST_MUTEX, SpinMutex<u64>, SpinMutex::new(42));
+        assert_eq!(*TEST_MUTEX.lock(), 42);
+        *TEST_MUTEX.lock() = 55;
+        assert_eq!(*TEST_MUTEX.lock(), 55);
     }
 }
