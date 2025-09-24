5219a52a17495d786cc6f0ee84102f6b8b1caf99
Balint Dobszay <balint.dobszay@arm.com>
2025-09-24 16:16:18 +0200

    fix(fvp): register usage in crash_console_flush
    The console_pl011_core_flush function expects the UART base address in
    x0, not x1. This caused an infinite recursion when the assert in this
    function failed.
    
    Change-Id: Ie5baabb935581f3576a1f754acde3a10dd3cc443
    Signed-off-by: Balint Dobszay <balint.dobszay@arm.com>

--- src/platform/fvp.rs
+++ src/platform/fvp.rs
@@ -396,7 +396,7 @@ unsafe impl Platform for Fvp {
     extern "C" fn crash_console_flush() {
         naked_asm!(
             include_str!("../asm_macros_common.S"),
-            "mov_imm	x1, {PLAT_ARM_CRASH_UART_BASE}",
+            "mov_imm	x0, {PLAT_ARM_CRASH_UART_BASE}",
             "b	console_pl011_core_flush",
             include_str!("../asm_macros_common_purge.S"),
             DEBUG = const DEBUG as i32,
