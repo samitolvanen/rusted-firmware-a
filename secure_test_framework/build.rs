1cbab2248ae698c79abdd316943c6fe557a19ec6
Ludovic Mermod <ludovic.mermod@arm.com>
2025-10-01 14:37:08 +0200

    refactor: add clippy for STF
    Run clippy on STF in `make clippy`. This change also fixes a bunch of
    errors and warnings reported by clippy, without affecting
    functionality.
    
    Change-Id: Ie51d2f3ad0685d5a6197fc3d155f1533ff6af93f
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- secure_test_framework/build.rs
+++ secure_test_framework/build.rs
@@ -17,22 +17,13 @@ fn main() {
     );
     println!("cargo:rustc-link-arg=-Timage.ld");
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!(
-        "cargo:rustc-link-arg-bin=bl32=-T{}/{}_bl32.ld",
-        crate_dir, platform
-    );
-    println!(
-        "cargo:rustc-link-arg-bin=bl33=-T{}/{}_bl33.ld",
-        crate_dir, platform
-    );
-    println!("cargo:rerun-if-changed={}/{}_bl32.ld", crate_dir, platform);
-    println!("cargo:rerun-if-changed={}/{}_bl33.ld", crate_dir, platform);
+    println!("cargo:rustc-link-arg-bin=bl32=-T{crate_dir}/{platform}_bl32.ld");
+    println!("cargo:rustc-link-arg-bin=bl33=-T{crate_dir}/{platform}_bl33.ld");
+    println!("cargo:rerun-if-changed={crate_dir}/{platform}_bl32.ld");
+    println!("cargo:rerun-if-changed={crate_dir}/{platform}_bl33.ld");
 
     if platform == "fvp" {
-        println!(
-            "cargo:rustc-link-arg-bin=realm=-T{}/{}_realm.ld",
-            crate_dir, platform
-        );
-        println!("cargo:rerun-if-changed={}/{}_realm.ld", crate_dir, platform);
+        println!("cargo:rustc-link-arg-bin=realm=-T{crate_dir}/{platform}_realm.ld");
+        println!("cargo:rerun-if-changed={crate_dir}/{platform}_realm.ld");
     }
 }
