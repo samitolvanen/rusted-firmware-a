5da2698887b32e03799d7f604cbd88113dfccc3e
Mateusz Sulimowicz <matsul@google.com>
2025-10-21 10:15:38 +0000

    feat(extensions): trace filtering control
    Change-Id: If22a99c7ae622c1a78b51c7dcb25c74e1cd1c805

--- src/cpu_extensions.rs
+++ src/cpu_extensions.rs
@@ -5,6 +5,7 @@
 //! A framework for managing ARM architectural CPU extensions using a trait-based approach.
 
 pub mod sys_reg_trace;
+pub mod trf;
 
 use crate::{
     context::{CpuContext, PerWorldContext, World},
