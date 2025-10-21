d0bc6a31b742088de674019e948d5e008adfdd03
Andrew Walbran <qwandor@google.com>
2025-10-21 18:25:25 +0100

    refactor: move register dump functions to Platform trait
    Change-Id: I39ddd14098c5499aee09ad011c8d669f2146f54c
    Signed-off-by: Andrew Walbran <qwandor@google.com>

--- src/platform.rs
+++ src/platform.rs
@@ -75,9 +75,9 @@ pub type PlatformServiceImpl = <PlatformImpl as Platform>::PlatformServiceImpl;
 /// only clobbers registers x0-x5. For any valid MPIDR value it must always return an index less than
 /// `CORE_COUNT`, and must return a different index for different MPIDR values.
 ///
-/// The implementations of `cold_boot_handler`, `crash_console_init`, `crash_console_putc` and
-/// `crash_console_flush` must be naked functions which doesn't use the stack, and only clobber the
-/// registers they are documented to clobber.
+/// The implementations of `cold_boot_handler`, `crash_console_init`, `crash_console_putc`,
+/// `crash_console_flush` and `dump_registers` must be naked functions which doesn't use the stack,
+/// and only clobber the registers they are documented to clobber.
 ///
 /// (These requirements don't apply to the test platform, as it is only used in unit tests.)
 pub unsafe trait Platform {
@@ -219,6 +219,18 @@ pub unsafe trait Platform {
     /// May clobber x0-x1.
     #[cfg_attr(test, allow(unused))]
     extern "C" fn crash_console_flush();
+
+    /// Dumps platform-specific registers, e.g. for the GIC, for a crash dump.
+    ///
+    /// This may be called without a Rust runtime, e.g. with no stack.
+    ///
+    /// May clobber x0-x11, x16, x17, sp.
+    ///
+    /// # Safety
+    ///
+    /// Should only be called from assembly as it doesn't follow the standard calling convention.
+    #[cfg_attr(test, allow(unused))]
+    unsafe extern "C" fn dump_registers();
 }
 
 #[cfg(all(target_arch = "aarch64", not(test)))]
