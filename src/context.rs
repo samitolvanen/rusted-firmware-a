bd143b91a29df746446a2e25fa4ee73cde248edd
Mateusz Sulimowicz <matsul@google.com>
2025-10-22 16:56:20 +0000

    feat(extensions): configure PMU
    Change-Id: I114a71f2707ad6b2652d5834a73c916eddd3a366
    Signed-off-by: Mateusz Sulimowicz <matsul@google.com>

--- src/context.rs
+++ src/context.rs
@@ -4,6 +4,7 @@
 
 use crate::{
     aarch64::isb,
+    cpu_extensions::pmuv3,
     gicv3,
     platform::{Platform, PlatformImpl, exception_free},
     smccc::SmcReturn,
@@ -669,6 +670,8 @@ fn initialise_common(context: &mut CpuContext, entry_point: &EntryPointInfo) {
     {
         context.el1_sysregs.sctlr_el1 = SctlrEl1::RES1;
     }
+
+    pmuv3::configure_per_cpu(context);
 }
 
 /// Initialises the given CPU context ready for booting NS-EL2 or NS-EL1.
