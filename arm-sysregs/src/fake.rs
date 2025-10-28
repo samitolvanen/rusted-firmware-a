a9e1bba2c4d105d7abc36cc34c676f91bc37eb4e
Jamie Fox <jamiefox@google.com>
2025-10-28 18:33:48 +0000

    feat(extensions): add RAS
    Adds the Reliability, Availability and Serviceability (RAS) extension.
    
    Change-Id: Icc2996d02196cbdc17f8f9655ee442f160a10128
    Signed-off-by: Jamie Fox <jamiefox@google.com>

--- arm-sysregs/src/fake.rs
+++ arm-sysregs/src/fake.rs
@@ -6,7 +6,7 @@
 
 use super::{
     ClidrEl1, CsselrEl1, CtrEl0, Esr, HcrEl2, HcrxEl2, IccSre, IdAa64dfr0El1, IdAa64mmfr1El1,
-    IdAa64mmfr2El1, MpidrEl1, ScrEl3, SctlrEl1, SctlrEl2, SctlrEl3, Spsr,
+    IdAa64mmfr2El1, IdAa64pfr0El1, MpidrEl1, ScrEl3, SctlrEl1, SctlrEl2, SctlrEl3, Spsr,
 };
 use std::sync::Mutex;
 
@@ -194,6 +194,8 @@ pub struct SystemRegisters {
     pub id_aa64mmfr1_el1: IdAa64mmfr1El1,
     /// Fake value for the ID_AA64MMFR2_EL1 system register.
     pub id_aa64mmfr2_el1: IdAa64mmfr2El1,
+    /// Fake value for the ID_AA64PFR0_EL1 system register.
+    pub id_aa64pfr0_el1: IdAa64pfr0El1,
     /// Fake value for the ISR_EL1 system register.
     pub isr_el1: u64,
     /// Fake value for the MAIR_EL1 system register.
@@ -260,10 +262,14 @@ pub struct SystemRegisters {
     pub vbar_el1: usize,
     /// Fake value for THEVBAR_EL2 system register.
     pub vbar_el2: usize,
+    /// Fake value for the VDISR_EL2 system register.
+    pub vdisr_el2: u64,
     /// Fake value for THEVMPIDR_EL2 system register.
     pub vmpidr_el2: u64,
     /// Fake value for THEVPIDR_EL2 system register.
     pub vpidr_el2: u64,
+    /// Fake value for the VSESR_EL2 system register.
+    pub vsesr_el2: u64,
     /// Fake value for THEVTCR_EL2 system register.
     pub vtcr_el2: u64,
     /// Fake value for THEVTTBR_EL2 system register.
@@ -311,6 +317,7 @@ impl SystemRegisters {
             id_aa64dfr0_el1: IdAa64dfr0El1::empty(),
             id_aa64mmfr1_el1: IdAa64mmfr1El1::empty(),
             id_aa64mmfr2_el1: IdAa64mmfr2El1::empty(),
+            id_aa64pfr0_el1: IdAa64pfr0El1::empty(),
             isr_el1: 0,
             mair_el1: 0,
             mair_el2: 0,
@@ -344,8 +351,10 @@ impl SystemRegisters {
             ttbr1_el2: 0,
             vbar_el1: 0,
             vbar_el2: 0,
+            vdisr_el2: 0,
             vmpidr_el2: 0,
             vpidr_el2: 0,
+            vsesr_el2: 0,
             vtcr_el2: 0,
             vttbr_el2: 0,
         }
---
+++ src/cpu_extensions/ras/ras_sel2.rs
@@ -0,0 +1,49 @@
+// Copyright The Rusted Firmware-A Contributors.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+//! FEAT_RAS context management for when Secure EL2 is enabled.
+
+use crate::{
+    context::{CPU_DATA_CONTEXT_NUM, PerCoreState, PerWorld, World},
+    platform::{Platform, PlatformImpl, exception_free},
+};
+use arm_sysregs::{read_vdisr_el2, read_vsesr_el2, write_vdisr_el2, write_vsesr_el2};
+use core::cell::RefCell;
+use percore::{ExceptionLock, PerCore};
+
+struct RasCpuContext {
+    vdisr_el2: u64,
+    vsesr_el2: u64,
+}
+
+impl RasCpuContext {
+    const EMPTY: Self = Self {
+        vdisr_el2: 0,
+        vsesr_el2: 0,
+    };
+}
+
+static RAS_CTX: PerCoreState<PerWorld<RasCpuContext>> = PerCore::new(
+    [const {
+        ExceptionLock::new(RefCell::new(PerWorld(
+            [RasCpuContext::EMPTY; CPU_DATA_CONTEXT_NUM],
+        )))
+    }; PlatformImpl::CORE_COUNT],
+);
+
+pub fn save_context(world: World) {
+    exception_free(|token| {
+        let mut ctx = RAS_CTX.get().borrow_mut(token);
+        ctx[world].vdisr_el2 = read_vdisr_el2();
+        ctx[world].vsesr_el2 = read_vsesr_el2();
+    })
+}
+
+pub fn restore_context(world: World) {
+    exception_free(|token| {
+        let ctx = RAS_CTX.get().borrow_mut(token);
+        write_vdisr_el2(ctx[world].vdisr_el2);
+        write_vsesr_el2(ctx[world].vsesr_el2);
+    })
+}
