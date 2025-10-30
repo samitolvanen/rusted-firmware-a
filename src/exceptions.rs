0662024ad02a45e386a58593def289ccb1990099
Mateusz Sulimowicz <matsul@google.com>
2025-10-30 16:43:37 +0000

    feat(extensions): support VHE
    Adds VirtualizationHost CpuExtension (FEAT_VHE support)
    
    Enabling VirtualizationHost enables VHE context save/restore
    if SEL2 is enabled.
    
    Change-Id: I6c0b6f1695538d4fbdfaecc094a817a28fe605fb

--- src/exceptions.rs
+++ src/exceptions.rs
@@ -4,13 +4,14 @@
 
 use crate::{
     context::{PER_WORLD_CONTEXT, World, cpu_state, world_context},
+    cpu_extensions::{CpuExtension, vhe::VirtualizationHost},
     platform::exception_free,
     smccc::SmcReturn,
 };
 use arm_sysregs::{
     Esr, ExceptionLevel, HcrEl2, ScrEl3, SctlrEl1, SctlrEl2, Spsr, StackPointer, read_hcr_el2,
-    read_id_aa64mmfr1_el1, read_sctlr_el1, read_sctlr_el2, read_vbar_el1, read_vbar_el2,
-    write_elr_el1, write_elr_el2, write_esr_el1, write_esr_el2, write_spsr_el1, write_spsr_el2,
+    read_sctlr_el1, read_sctlr_el2, read_vbar_el1, read_vbar_el2, write_elr_el1, write_elr_el2,
+    write_esr_el1, write_esr_el2, write_spsr_el1, write_spsr_el2,
 };
 #[cfg(not(test))]
 use core::arch::asm;
@@ -91,7 +92,7 @@ fn find_exception_vector(spsr_el3: Spsr, vbar: usize, target_el: ExceptionLevel)
 }
 
 fn is_tge_enabled() -> bool {
-    read_id_aa64mmfr1_el1().is_feat_vhe_present() && read_hcr_el2().contains(HcrEl2::TGE)
+    VirtualizationHost.is_present() && read_hcr_el2().contains(HcrEl2::TGE)
 }
 
 /// Returns whether we are in secure state on a system without S-EL2.
