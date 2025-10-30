d72773a6255f4c647199ffff3ec7b11e7d8af248
Jamie Fox <jamiefox@google.com>
2025-10-30 17:23:27 +0000

    feat(extensions): extended translation control (TCR2)
    Change-Id: I09aca09d685ba7c80a554ef305f16d7b635fc19f
    Signed-off-by: Jamie Fox <jamiefox@google.com>

---
+++ src/cpu_extensions/tcr2/tcr2_sel2.rs
@@ -0,0 +1,43 @@
+// Copyright The Rusted Firmware-A Contributors.
+//
+// SPDX-License-Identifier: BSD-3-Clause
+
+//! FEAT_TCR2 context management for when Secure EL2 is enabled.
+
+use crate::{
+    context::{CPU_DATA_CONTEXT_NUM, PerCoreState, PerWorld, World},
+    platform::{Platform, PlatformImpl, exception_free},
+};
+use arm_sysregs::{read_tcr2_el2, write_tcr2_el2};
+use core::cell::RefCell;
+use percore::{ExceptionLock, PerCore};
+
+struct Tcr2CpuContext {
+    tcr2_el2: u64,
+}
+
+impl Tcr2CpuContext {
+    const EMPTY: Self = Self { tcr2_el2: 0 };
+}
+
+static TCR2_CTX: PerCoreState<PerWorld<Tcr2CpuContext>> = PerCore::new(
+    [const {
+        ExceptionLock::new(RefCell::new(PerWorld(
+            [Tcr2CpuContext::EMPTY; CPU_DATA_CONTEXT_NUM],
+        )))
+    }; PlatformImpl::CORE_COUNT],
+);
+
+pub fn save_context(world: World) {
+    exception_free(|token| {
+        let mut ctx = TCR2_CTX.get().borrow_mut(token);
+        ctx[world].tcr2_el2 = read_tcr2_el2();
+    })
+}
+
+pub fn restore_context(world: World) {
+    exception_free(|token| {
+        let ctx = TCR2_CTX.get().borrow_mut(token);
+        write_tcr2_el2(ctx[world].tcr2_el2);
+    })
+}
