42c7e6949ace497c8e8609828c1474dc8de03f91
Jim Ray <jimray@google.com>
2025-11-04 19:06:58 +0000

    feat(osi): Add SET_SUSPEND_MODE SMC and advertise OSI
    This change begins to add support for OS-initiated (OSI) mode.
    OS-initiated mode is described in the PSCI spec (DEN0022F.b) and on the
    following page:
    https://trustedfirmware-a.readthedocs.io/en/latest/design_documents/psci_osi_mode.html.
    
    Specifically, this change does the following: 1. It starts reporting OSI
    mode support if the platform supports it. This is done through the
    PSCI_FEATURES SMC as described in section 5.16.2 of the PSCI spec.
    
    2. It implements the PSCI_SET_SUSPEND_MODE SMC call (see section 5.21 of
    the PSCI spec).
    
    Platforms can declare their support of OSI mode through the FEATURES
    variable in their implementation of the PsciPlatformInterface trait.
    
    A simple test for the SET_SUSPEND_MODE SMC has been added to STF as well.
    
    Issue: #80
    Change-Id: I20dc4c0d097af5360db0b66beafa7a64cd4d94af
    Signed-off-by: Jim Ray <jimray@google.com>

--- secure_test_framework/src/tests/psci.rs
+++ secure_test_framework/src/tests/psci.rs
@@ -5,13 +5,33 @@
 //! Tests for PSCI calls.
 
 use crate::framework::{expect::expect_eq, normal_world_test, secure_world_test};
+use arm_psci::FeatureFlagsCpuSuspend;
 use smccc::{Smc, psci};
+
+fn is_osi_supported() -> Result<bool, ()> {
+    let suspend_features = psci::psci_features::<Smc>(psci::PSCI_CPU_SUSPEND_64).map_err(|_| ())?;
+    let flags = FeatureFlagsCpuSuspend::from_bits(suspend_features).ok_or(())?;
+    Ok(flags.intersects(FeatureFlagsCpuSuspend::OS_INITIATED_MODE))
+}
 
 normal_world_test!(test_psci_version);
 fn test_psci_version() -> Result<(), ()> {
     expect_eq!(
         psci::version::<Smc>(),
         Ok(psci::Version { major: 1, minor: 3 })
+    );
+    Ok(())
+}
+
+normal_world_test!(test_set_suspend_mode_to_osi);
+fn test_set_suspend_mode_to_osi() -> Result<(), ()> {
+    let has_osi_support = is_osi_supported()?;
+    if !has_osi_support {
+        return Ok(());
+    }
+    expect_eq!(
+        psci::set_suspend_mode::<Smc>(psci::SuspendMode::OsInitiated),
+        Ok(())
     );
     Ok(())
 }
