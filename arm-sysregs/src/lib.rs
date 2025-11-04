816ec03bd77ba76babd3d3659c0f7a2c488750fc
Jamie Fox <jamiefox@google.com>
2025-11-04 12:53:37 +0000

    feat(extensions): data independent timing (DIT)
    Reintroduce the assembly that enables DIT on entry to EL3 and make it
    unconditional, since FEAT_DIT is mandatory from Armv8.4.
    
    Change-Id: Ifc1c6bf94bcafee514ab6bd1509b17ba91feb562
    Signed-off-by: Jamie Fox <jamiefox@google.com>

--- arm-sysregs/src/lib.rs
+++ arm-sysregs/src/lib.rs
@@ -572,6 +572,14 @@ bitflags! {
         const EZ = 1 << 8;
     }
 
+    /// DIT (Data Independent Timing) register value.
+    #[derive(Clone, Copy, Debug, Eq, PartialEq, Default)]
+    #[repr(transparent)]
+    pub struct Dit: u64 {
+        /// Enable data independent timing.
+        const DIT = 1 << 24;
+    }
+
     /// PMCR_EL0 register value.
     #[derive(Clone, Copy, Debug, Eq, PartialEq, Default)]
     #[repr(transparent)]
