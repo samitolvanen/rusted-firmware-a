f2ed28c65d2f7eeb7dafd754f0f839661f35cabe
Mateusz Sulimowicz <matsul@google.com>
2025-11-06 12:12:54 +0000

    feat(extensions): configure traps
    By default:
    - Trap EL2 accesses to CPTR_EL2/HCPTR, and EL2/EL1 accesses to
      CPACR_EL1/CPACR
    - When FEAT_AMUv1 implemented and, trap accesses from EL2/EL1/EL0 to
      AMU registers
    - Trap trace system register accesses
    - Trap Advanced SIMD instructions execution
    
    Change-Id: Ia9d9a91fe4a7a9a4484f54c4a8532d7258355080
    Signed-off-by: Mateusz Sulimowicz <matsul@google.com>

--- src/cpu_extensions/mpam.rs
+++ src/cpu_extensions/mpam.rs
@@ -24,7 +24,7 @@ use crate::context::{PerWorldContext, World};
 use arm_sysregs::{Mpam3El3, read_id_aa64pfr0_el1};
 
 /// FEAT_MPAM support
-/// 
+///
 /// Enables MPAM configuration adn disables MPAM system register traps for NS and Realm worlds.
 pub struct Mpam;
 
