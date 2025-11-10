2fcdaf5d12f1eb9e58a2f9c89d85fcb58d3e1abb
Ludovic Mermod <ludovic.mermod@arm.com>
2025-11-10 14:41:58 +0100

    feat(rme): allocate boot manifest for R-EL2 payload
    When RME is enabled, EL3 will allocate shared buffer before booting to
    communicate with the R-EL2 payload and initialize the RMM Boot Manifest
    at its start.
    
    This change also adds data structure definitions to manipulate the Boot
    Manifest from within EL3. This could later be moved to a dedicated crate
    if pertinent.
    
    Change-Id: Ibd3cf332662d0d58f4d7d7accf7de114887dbd91
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- src/platform/fvp.rs
+++ src/platform/fvp.rs
@@ -6,6 +6,10 @@ mod config;
 
 use self::config::{FVP_CLUSTER_COUNT, FVP_MAX_CPUS_PER_CLUSTER, FVP_MAX_PE_PER_CPU};
 use super::{DummyService, Platform};
+#[cfg(feature = "rme")]
+use crate::services::rmmd::manifest::{
+    ManifestList, RmmBootManifest, RmmConsoleInfo, RmmMemoryBank,
+};
 use crate::{
     aarch64::{dsb_ish, dsb_sy, wfi},
     context::{CoresImpl, EntryPointInfo},
@@ -106,14 +110,9 @@ const NT_FW_CONFIG_ADDRESS: u64 = 0x8000_0000;
 const HW_CONFIG_ADDRESS: u64 = 0x07f0_0000;
 const HW_CONFIG_ADDRESS_NS: u64 = 0x8200_0000;
 
-// TODO: Use the correct values here (see services/std_svc/rmmd/rmmd_main.c).
 /// Version of the RMM Boot Interface.
 #[cfg(feature = "rme")]
-const RMM_BOOT_VERSION: u64 = 0;
-/// Base address for the EL3 - RMM shared area. The boot manifest should be stored at the beginning
-/// of this area.
-#[cfg(feature = "rme")]
-const RMM_SHARED_AREA_BASE_ADDRESS: u64 = 0;
+const RMM_BOOT_VERSION: u64 = 0x4;
 
 const EARLY_REGIONS: [EarlyRegion; 2] = [
     EarlyRegion {
@@ -173,6 +172,37 @@ pub struct Fvp;
 unsafe impl Platform for Fvp {
     const CORE_COUNT: usize = PLATFORM_CORE_COUNT;
     const CACHE_WRITEBACK_GRANULE: usize = 1 << 6;
+    const PAGE_HEAP_PAGE_COUNT: usize = 6;
+
+    #[cfg(feature = "rme")]
+    const RMM_SHARED_BUFFER_START: usize = 0xffbf_f000;
+
+    #[cfg(feature = "rme")]
+    fn rme_prepare_manifest(buf: &mut [u8]) {
+        let manifest = RmmBootManifest::new(buf, 2, 1, 0, 0, 0, &[]);
+
+        // TODO: These addresses should be parsed from FW_CONFIG
+        manifest.plat_dram.as_slice_mut()[0] = RmmMemoryBank {
+            base: NT_FW_CONFIG_ADDRESS,
+            size: 0x7c00_0000,
+        };
+        manifest.plat_dram.as_slice_mut()[1] = RmmMemoryBank {
+            base: 0x0008_8000_0000,
+            size: 0x0000_8000_0000,
+        };
+
+        manifest.plat_console.as_slice_mut()[0] = RmmConsoleInfo {
+            // Value from the pl011_uart crate.
+            base: 0x1C09_0000,
+
+            // Values from TF-A.
+            map_pages: 0x1,
+            name: [0x70, 0x6c, 0x30, 0x31, 0x31, 0x0, 0x0, 0x0], // "pl011"
+            clk_in_hz: 0x00e1_0000,
+            baud_rate: 0x1c200,
+            flags: 0,
+        };
+    }
 
     type LogSinkImpl = LockedWriter<Uart<'static>>;
     type PsciPlatformImpl = FvpPsciPlatformImpl<'static>;
@@ -298,7 +328,7 @@ unsafe impl Platform for Fvp {
                 core_linear_id,
                 RMM_BOOT_VERSION,
                 Self::CORE_COUNT as u64,
-                RMM_SHARED_AREA_BASE_ADDRESS,
+                Self::RMM_SHARED_BUFFER_START as u64,
                 0,
                 0,
                 0,
--- src/services/rmmd.rs
+++ src/services/rmmd.rs
@@ -2,12 +2,59 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
+use core::ptr::slice_from_raw_parts_mut;
+
 use crate::{
     context::World,
     info,
+    layout::{rmm_shared_end, rmm_shared_start},
+    platform::{Platform, PlatformImpl},
     services::{Service, owns},
     smccc::{FunctionId, NOT_SUPPORTED, OwningEntityNumber, SmcReturn},
 };
+
+pub mod manifest;
+
+/// Returns a mutable reference to the shared buffer used for communication between R-EL2 and EL3.
+///
+/// ## Safety
+///
+/// Calling this function is always safe, but using its return value is safe if all the conditions
+/// below are met:
+///
+/// - It can only be called after the shared buffer is mapped into the page table.
+/// - After calling `get_shared_buffer`, the return reference must be dropped before any other call
+///   to it is made.
+/// - The reference must be dropped before switching to Realm World.
+unsafe fn get_shared_buffer() -> &'static mut [u8] {
+    // Safety: (relative to [`slice::from_raw_parts_mut`][https://doc.rust-lang.org/stable/core/slice/fn.from_raw_parts_mut.html])
+    // - The first condition of `get_shared_buffer()` ensures that the location is valid, and as it
+    //   occupies exactly one page, it will always be aligned.
+    // - `u8` is properly initialized regardless of the initial value.
+    // - The second condition ensures that the buffer is never accessed through multiple reference
+    //   within EL3. As it can only be accessed by EL3 and Realm World, it follows from the third
+    //   condition that no other pointers can be used to access the buffer while a reference exists.
+    // - Follows from the soundness of the layout defined in `layout.rs`.
+    unsafe {
+        &mut *slice_from_raw_parts_mut(
+            rmm_shared_start() as *mut u8,
+            rmm_shared_end() - rmm_shared_start(),
+        )
+    }
+}
+
+pub fn rme_prepare() {
+    // Safety:
+    // - This function is called after initializing the MMU and pagetable.
+    // - This function never calls again `get_shared_buffer()`, thus the reference will be dropped
+    //   upon return, before another call is made.
+    // - Similarly to the above, this function does not switch to the Realm World.
+    let buf = unsafe { get_shared_buffer() };
+
+    PlatformImpl::rme_prepare_manifest(buf);
+
+    info!("RME Boot Manifest ready")
+}
 
 const RMM_BOOT_COMPLETE: u32 = 0xC400_01CF;
 
