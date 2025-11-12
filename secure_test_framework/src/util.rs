9753e61b785cade084ec2f655ccdd6060f6eb17c
Ludovic Mermod <ludovic.mermod@arm.com>
2025-11-12 14:51:03 +0100

    refactor: add clippy for STF
    Run clippy on STF in `make clippy`. This change also fixes a bunch of
    errors and warnings reported by clippy, without affecting
    functionality.
    
    Change-Id: Ie51d2f3ad0685d5a6197fc3d155f1533ff6af93f
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- secure_test_framework/src/util.rs
+++ secure_test_framework/src/util.rs
@@ -59,7 +59,7 @@ pub fn enable_pauth(key: u128) {
 ///
 /// This is convenient for handling errors which should cause a test to fail.
 pub fn log_error<V, E: Display>(message: &str, result: Result<V, E>) -> Result<V, ()> {
-    result.map_err(|e| error!("{}: {}", message, e))
+    result.map_err(|e| error!("{message}: {e}"))
 }
 
 /// If the given FF-A response is a success then returns its arguments, otherwise logs and returns
@@ -68,7 +68,7 @@ pub fn expect_ffa_success(response: Interface) -> Result<SuccessArgs, ()> {
     if let Interface::Success { args, .. } = response {
         Ok(args)
     } else {
-        error!("Expected success but got {:?}", response);
+        error!("Expected success but got {response:?}");
         Err(())
     }
 }
@@ -83,7 +83,7 @@ pub fn expect_ffa_mem_retrieve_resp(response: Interface) -> Result<(u32, u32), (
     {
         Ok((total_len, frag_len))
     } else {
-        error!("Expected MemRetrieveResp but got {:?}", response);
+        error!("Expected MemRetrieveResp but got {response:?}");
         Err(())
     }
 }
