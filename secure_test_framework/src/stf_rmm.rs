3f19e8a2387912b348dfe4cd01eea4f437e3bce9
Ludovic Mermod <ludovic.mermod@arm.com>
2025-11-12 10:52:22 +0100

    feat(rme): forward RMI calls to R-EL2
    Implement forwarding for RMI SMCs coming from NS World to Realm
    World, and vice versa for return values.
    
    In addition, implement the `RMM_RMI_REQ_VERSION` function in the
    realm image and add a new rudimentary test suite calling this
    function from NS World.
    
    Change-Id: I1f3b6f73a09f2971e1892926235aaba7fb71fa0c
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- secure_test_framework/src/stf_rmm.rs
+++ secure_test_framework/src/stf_rmm.rs
@@ -35,6 +35,8 @@ const SUPPORTED_RMM_MANIFEST_VERSION: RmmBootManifestVersion =
     RmmBootManifestVersion { major: 0, minor: 5 };
 
 const RMM_BOOT_COMPLETE: u64 = 0xC400_01CF;
+const RMM_RMI_REQ_COMPLETE: u64 = 0xC400_018F;
+const RMM_RMI_REQ_VERSION: u64 = 0xC400_0150;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 struct RmmBootManifestVersion {
@@ -84,10 +86,54 @@ fn complete_boot(ret: RmmBootReturn) -> ! {
     // Sends a RMM_BOOT_COMPLETED SMC to notify the Root World that RMM has booted.
     let mut args: [u64; 17] = [0; 17];
     args[0] = ret;
-    smc64(RMM_BOOT_COMPLETE as u32, args);
+    let regs = smc64(RMM_BOOT_COMPLETE as u32, args);
 
-    // TODO: handle RMI calls originating from NS world.
-    todo!()
+    handle_incoming_calls(regs)
+}
+
+/// Infinite loop to handle RMI calls coming from NS World.
+fn handle_incoming_calls(mut regs: [u64; 18]) -> ! {
+    info!("Received RMI call for FID 0x{:X}", regs[0]);
+
+    loop {
+        let ret = handle_rmi_call(&regs);
+
+        regs = smc64(RMM_RMI_REQ_COMPLETE as u32, ret);
+    }
+}
+
+/// Handles a single RMI call from NS world.
+fn handle_rmi_call(regs: &[u64]) -> [u64; 17] {
+    match regs[0] {
+        RMM_RMI_REQ_VERSION => rmi_version(regs),
+        _ => {
+            let mut ret = [0; 17];
+            ret[0] = u64::MAX;
+            ret
+        }
+    }
+}
+
+fn rmi_version(args: &[u64]) -> [u64; 17] {
+    let mut ret = [0; 17];
+
+    let Ok(requested) = RmmBootManifestVersion::try_from(args[1] as u32) else {
+        ret[0] = 1;
+        return ret;
+    };
+
+    info!(
+        "Received RMM_RMI_REQ_VERSION for v{}.{}",
+        requested.major, requested.minor
+    );
+
+    let supported = u32::from(SUPPORTED_RMM_VERSION) as u64;
+
+    ret[0] = if args[1] == supported { 0 } else { 1 };
+    ret[1] = supported;
+    ret[2] = supported;
+
+    ret
 }
 
 fn validate_args(pe_idx: u64, version: u64, core_count: u64, shared_buffer_addr: u64) {
