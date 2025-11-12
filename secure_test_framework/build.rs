d3c6647333c58700eb5da1b3a9928d6969d2578d
Ludovic Mermod <ludovic.mermod@arm.com>
2025-11-12 14:51:03 +0100

    refactor: add clippy for STF
    Run clippy on STF in `make clippy`. This change also fixes a bunch of
    errors and warnings reported by clippy, without affecting
    functionality.
    
    Change-Id: Ie51d2f3ad0685d5a6197fc3d155f1533ff6af93f
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- secure_test_framework/build.rs
+++ secure_test_framework/build.rs
@@ -17,26 +17,14 @@ fn main() {
     );
     println!("cargo:rustc-link-arg=-Timage.ld");
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    println!(
-        "cargo:rustc-link-arg-bin=bl32=-T{}/{}_bl32.ld",
-        crate_dir, platform
-    );
-    println!(
-        "cargo:rustc-link-arg-bin=bl33=-T{}/{}_bl33.ld",
-        crate_dir, platform
-    );
-    println!("cargo:rerun-if-changed={}/{}_bl32.ld", crate_dir, platform);
-    println!("cargo:rerun-if-changed={}/{}_bl33.ld", crate_dir, platform);
+    println!("cargo:rustc-link-arg-bin=bl32=-T{crate_dir}/{platform}_bl32.ld");
+    println!("cargo:rustc-link-arg-bin=bl33=-T{crate_dir}/{platform}_bl33.ld");
+    println!("cargo:rerun-if-changed={crate_dir}/{platform}_bl32.ld");
+    println!("cargo:rerun-if-changed={crate_dir}/{platform}_bl33.ld");
 
     #[cfg(feature = "rme")]
     {
-        println!(
-            "cargo:rustc-link-arg-bin=stf_rmm=-T{}/{}_stf_rmm.ld",
-            crate_dir, platform
-        );
-        println!(
-            "cargo:rerun-if-changed={}/{}_stf_rmm.ld",
-            crate_dir, platform
-        );
+        println!("cargo:rustc-link-arg-bin=stf_rmm=-T{crate_dir}/{platform}_stf_rmm.ld");
+        println!("cargo:rerun-if-changed={crate_dir}/{platform}_stf_rmm.ld");
     }
 }
--- secure_test_framework/src/tests/ffa_spmd.rs
+++ secure_test_framework/src/tests/ffa_spmd.rs
@@ -707,8 +707,8 @@ fn mem_retrieve_req_handler(interface: Interface) -> Option<Interface> {
     assert_eq!(buf, None);
 
     Some(Interface::MemRetrieveResp {
-        total_len: total_len,
-        frag_len: frag_len,
+        total_len,
+        frag_len,
     })
 }
 
@@ -802,7 +802,7 @@ fn notification_bind_handler(interface: Interface) -> Option<Interface> {
 
     assert_eq!(sender_id, 0x8003);
     assert_eq!(receiver_id, 0x8004);
-    assert_eq!(bitmap, 0x1051006008009030);
+    assert_eq!(bitmap, 0x1051_0060_0800_9030);
     assert_eq!(
         flags,
         NotificationBindFlags {
@@ -833,7 +833,7 @@ fn test_ffa_notification_bind() -> Result<(), ()> {
             NotificationBindFlags {
                 per_vcpu_notification: false,
             },
-            0x1051006008009030,
+            0x1051_0060_0800_9030,
         )
     );
 
@@ -901,7 +901,7 @@ fn notification_set_handler(interface: Interface) -> Option<Interface> {
             vcpu_id: Some(5)
         }
     );
-    assert_eq!(bitmap, 0x0051006388009530);
+    assert_eq!(bitmap, 0x0051_0063_8800_9530);
 
     Some(Interface::Success {
         target_info: TargetInfo {
@@ -927,7 +927,7 @@ fn test_ffa_notification_set() -> Result<(), ()> {
                 delay_schedule_receiver: true,
                 vcpu_id: Some(5),
             },
-            0x0051006388009530,
+            0x0051_0063_8800_9530,
         )
     );
 
@@ -989,7 +989,7 @@ fn test_ffa_notification_get() -> Result<(), ()> {
     let args = expect_ffa_interface!(
         expect_ffa_success,
         "NOTIFICATION_GET failed",
-        ffa::notification_get(17, 44, notification_get_flags.clone(),)
+        ffa::notification_get(17, 44, notification_get_flags)
     );
 
     let args: Result<SuccessArgsNotificationGet, arm_ffa::Error> =
@@ -1015,7 +1015,7 @@ fn notification_info_get_handler(interface: Interface) -> Option<Interface> {
         return None;
     };
 
-    assert_eq!(is_32bit, false);
+    assert!(!is_32bit);
 
     Some(Interface::Success {
         target_info: TargetInfo {
