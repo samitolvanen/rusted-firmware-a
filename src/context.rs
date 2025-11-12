2b068b2a2e7ed97d8c3c638b713c31ca698a0c61
Ludovic Mermod <ludovic.mermod@arm.com>
2025-11-12 14:50:38 +0100

    feat(rme): add payload for R-EL2 & build instructions
    Introduces a new STF image which runs in the Realm World, instantly
    returning to the Root World with the SMC indicating a successful R-EL2
    boot.
    
    Realm worlds requires three extensions to be present: FGT2, TCR2 and
    HCX. This patch adds them to the FVP cpu extensions and along with the
    implementation for FGT2.
    
    Finally, this also updates the FVP arguments in `build-and-run.sh` when
    RME is enabled, based on the shrinkwrap configuration for running 4
    worlds.
    
    Change-Id: Ifc97c51d722d991bfaa1da1708aae8cceb9b1c8c
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- src/context.rs
+++ src/context.rs
@@ -685,7 +685,7 @@ fn initialise_common(context: &mut CpuContext, entry_point: &EntryPointInfo) {
     //
     // NOTE: Modifying EEL2 bit along with EA bit ensures that we mitigate
     // against ERRATA_V2_3099206.
-    context.el3_state.scr_el3 = ScrEl3::RES1 | ScrEl3::HCE | ScrEl3::SIF | ScrEl3::RW;
+    context.el3_state.scr_el3 = ScrEl3::RES1 | ScrEl3::HCE | ScrEl3::EA | ScrEl3::SIF | ScrEl3::RW;
     #[cfg(feature = "sel2")]
     {
         context.el3_state.scr_el3 |= ScrEl3::EEL2;
@@ -744,7 +744,8 @@ fn initialise_secure(context: &mut CpuContext, entry_point: &EntryPointInfo) {
 fn initialise_realm(context: &mut CpuContext, entry_point: &EntryPointInfo) {
     initialise_common(context, entry_point);
     // SCR_NS + SCR_NSE = Realm state
-    context.el3_state.scr_el3 |= ScrEl3::NS | ScrEl3::NSE;
+    context.el3_state.scr_el3 |=
+        ScrEl3::NS | ScrEl3::NSE | /* TODO: replace with a CPU extension */ ScrEl3::FGTEN;
 
     // Configure CPU extensions for the Realm world.
     for ext in PlatformImpl::CPU_EXTENSIONS {
