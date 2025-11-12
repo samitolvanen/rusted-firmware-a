0d59df34abb14922b2ba82ff0f8a775d99cec07f
Ludovic Mermod <ludovic.mermod@arm.com>
2025-11-12 11:00:01 +0100

    feat(rme): allocate boot manifest for R-EL2 payload
    When RME is enabled, EL3 will allocate shared buffer before booting to
    communicate with the R-EL2 payload and initialize the RMM Boot Manifest
    at its start.
    
    This change also adds data structure definitions to manipulate the Boot
    Manifest from within EL3. This could later be moved to a dedicated crate
    if pertinent.
    
    Change-Id: Ibd3cf332662d0d58f4d7d7accf7de114887dbd91
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- src/services/rmmd.rs
+++ src/services/rmmd.rs
@@ -2,12 +2,48 @@
 //
 // SPDX-License-Identifier: BSD-3-Clause
 
+use core::slice::from_raw_parts_mut;
+
 use crate::{
     context::World,
     info,
+    platform::{Platform, PlatformImpl},
     services::{Service, owns},
     smccc::{FunctionId, NOT_SUPPORTED, OwningEntityNumber, SmcReturn},
 };
+
+/// Size in bytes of the EL3 - RMM shared area.
+pub const RMM_SHARED_BUFFER_SIZE: usize = 0x1000;
+
+pub mod manifest;
+
+/// Returns a mutable reference to the shared buffer used for communication between R-EL2 and EL3.
+///
+/// # Safety
+///
+/// Calling this function is always safe, but using its return value is safe if all the conditions
+/// below are met:
+///
+/// - It can only be called after the shared buffer is mapped into the page table.
+/// - After calling `get_shared_buffer`, the return reference must be dropped before any other call
+///   to it is made.
+/// - The reference must be dropped before switching to Realm World.
+unsafe fn get_shared_buffer() -> &'static mut [u8] {
+    // Safety: (relative to [`slice::from_raw_parts_mut`][https://doc.rust-lang.org/stable/core/slice/fn.from_raw_parts_mut.html])
+    // - The first condition of `get_shared_buffer()` ensures that the location is valid, and as it
+    //   occupies exactly one page, it will always be aligned.
+    // - `u8` is properly initialized regardless of the initial value.
+    // - The second condition ensures that the buffer is never accessed through multiple reference
+    //   within EL3. As it can only be accessed by EL3 and Realm World, it follows from the third
+    //   condition that no other pointers can be used to access the buffer while a reference exists.
+    // - Follows from the soundness of the layout defined in `layout.rs`.
+    unsafe {
+        from_raw_parts_mut(
+            PlatformImpl::RMM_SHARED_BUFFER_START as *mut u8,
+            RMM_SHARED_BUFFER_SIZE,
+        )
+    }
+}
 
 const RMM_BOOT_COMPLETE: u32 = 0xC400_01CF;
 
@@ -64,6 +100,17 @@ impl Service for Rmmd {
 
 impl Rmmd {
     pub(super) fn new() -> Self {
+        // Safety:
+        // - This function is called after initializing the MMU and pagetable.
+        // - This function never calls again `get_shared_buffer()`, thus the reference will be dropped
+        //   upon return, before another call is made.
+        // - Similarly to the above, this function does not switch to the Realm World.
+        let buf = unsafe { get_shared_buffer() };
+
+        PlatformImpl::rme_prepare_manifest(buf);
+
+        info!("RMM Boot Manifest ready");
+
         Self
     }
 }
