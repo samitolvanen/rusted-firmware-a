8a6f8c0e07f228bfdc6d39cf02bdea4d4c93481f
Ludovic Mermod <ludovic.mermod@arm.com>
2025-11-13 09:00:35 +0100

    feat(rme): allocate boot manifest for R-EL2 payload
    When RME is enabled, EL3 will allocate shared buffer before booting to
    communicate with the R-EL2 payload and initialize the RMM Boot Manifest
    at its start.
    
    This change also adds data structure definitions to manipulate the Boot
    Manifest from within EL3. This could later be moved to a dedicated crate
    if pertinent.
    
    Change-Id: Ibd3cf332662d0d58f4d7d7accf7de114887dbd91
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- src/platform.rs
+++ src/platform.rs
@@ -36,6 +36,8 @@ select_platform!(platform = "fvp", fvp::Fvp);
 select_platform!(platform = "qemu", qemu::Qemu);
 select_platform!(test, test::TestPlatform);
 
+#[cfg(feature = "rme")]
+use crate::services::rmmd::RMM_SHARED_BUFFER_SIZE;
 use crate::{
     context::EntryPointInfo,
     cpu_extensions::CpuExtension,
@@ -102,6 +104,16 @@ pub unsafe trait Platform {
 
     /// The number of pages to reserve for the page heap.
     const PAGE_HEAP_PAGE_COUNT: usize = 5;
+
+    #[cfg(feature = "rme")]
+    /// Base address for the EL3 - RMM shared area.
+    const RMM_SHARED_BUFFER_START: usize;
+
+    #[cfg(feature = "rme")]
+    /// Platform dependent part of the RME Boot Manifest. Entries within the range `0..RMM_<NAME>`
+    /// (see above) are allocated to be filled by this function. Any extra entry is reserved for
+    /// platform independent data.
+    fn rme_prepare_manifest(_buf: &mut [u8; RMM_SHARED_BUFFER_SIZE]) {}
 
     /// Platform dependent LogSink implementation type for Logger.
     type LogSinkImpl: LogSink;
