c19b4337d6512e3a1fa3c6e9c9f5a5e065dc34e9
Ludovic Mermod <ludovic.mermod@arm.com>
2025-11-13 09:02:38 +0100

    feat(gpt): enable gpt before entering Realm World
    Change-Id: I8f5ef38716c546d8210e0901752bd008e9d3b4e1
    Signed-off-by: Ludovic Mermod <ludovic.mermod@arm.com>

--- arm-gpt/src/lib.rs
+++ arm-gpt/src/lib.rs
@@ -716,11 +716,13 @@ mod test {
         };
     }
 
+    #[derive(Debug)]
     enum L0Matcher<'a> {
         Block(GPIAccessType),
         Table(&'a [(usize, L1Matcher<'a>)]),
     }
 
+    #[derive(Debug)]
     enum L1Matcher<'a> {
         Contig(ContigSize, GPIAccessType),
         GranulePart(&'a [(usize, GPIAccessType)]),
@@ -1176,4 +1178,6 @@ mod test {
 
         Ok(())
     }
+
+    // TODO: regression test for matching Contig too small in compute_rewrite
 }
